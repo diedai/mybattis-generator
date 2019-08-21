04463475c4f9aa8e9b9fd88d0001259335adbc4e
gongzhihao <gongzhihao@192.168.1.45>
2019-08-21 11:50:48 +0800

    添加源码

---
+++ src/main/java/ctas/test/entity/Employee.java
@@ -0,0 +1,233 @@
+package ctas.test.entity;
+
+public class Employee {
+    private Integer id;
+
+    private String employeeNum;
+
+    private String name;
+
+    private String mobilePhone;
+
+    private Integer departmentId;
+
+    private Integer workCityId;
+
+    private Integer companyId;
+
+    private Boolean workStatus;
+
+    private Integer positionId;
+
+    private String nation;
+
+    private Boolean sex;
+
+    private Boolean ismarry;
+
+    private String email;
+
+    private String interest;
+
+    private String recommended;
+
+    private String professionalTitle;
+
+    private String identifyCard;
+
+    private String bankCard;
+
+    private String openBank;
+
+    private String comunicationAddr;
+
+    private String homeAddr;
+
+    private String urgencyContact;
+
+    private String urgencyContactTelephone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmployeeNum() {
+        return employeeNum;
+    }
+
+    public void setEmployeeNum(String employeeNum) {
+        this.employeeNum = employeeNum == null ? null : employeeNum.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone == null ? null : mobilePhone.trim();
+    }
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public Integer getWorkCityId() {
+        return workCityId;
+    }
+
+    public void setWorkCityId(Integer workCityId) {
+        this.workCityId = workCityId;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+
+    public Boolean getWorkStatus() {
+        return workStatus;
+    }
+
+    public void setWorkStatus(Boolean workStatus) {
+        this.workStatus = workStatus;
+    }
+
+    public Integer getPositionId() {
+        return positionId;
+    }
+
+    public void setPositionId(Integer positionId) {
+        this.positionId = positionId;
+    }
+
+    public String getNation() {
+        return nation;
+    }
+
+    public void setNation(String nation) {
+        this.nation = nation == null ? null : nation.trim();
+    }
+
+    public Boolean getSex() {
+        return sex;
+    }
+
+    public void setSex(Boolean sex) {
+        this.sex = sex;
+    }
+
+    public Boolean getIsmarry() {
+        return ismarry;
+    }
+
+    public void setIsmarry(Boolean ismarry) {
+        this.ismarry = ismarry;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getInterest() {
+        return interest;
+    }
+
+    public void setInterest(String interest) {
+        this.interest = interest == null ? null : interest.trim();
+    }
+
+    public String getRecommended() {
+        return recommended;
+    }
+
+    public void setRecommended(String recommended) {
+        this.recommended = recommended == null ? null : recommended.trim();
+    }
+
+    public String getProfessionalTitle() {
+        return professionalTitle;
+    }
+
+    public void setProfessionalTitle(String professionalTitle) {
+        this.professionalTitle = professionalTitle == null ? null : professionalTitle.trim();
+    }
+
+    public String getIdentifyCard() {
+        return identifyCard;
+    }
+
+    public void setIdentifyCard(String identifyCard) {
+        this.identifyCard = identifyCard == null ? null : identifyCard.trim();
+    }
+
+    public String getBankCard() {
+        return bankCard;
+    }
+
+    public void setBankCard(String bankCard) {
+        this.bankCard = bankCard == null ? null : bankCard.trim();
+    }
+
+    public String getOpenBank() {
+        return openBank;
+    }
+
+    public void setOpenBank(String openBank) {
+        this.openBank = openBank == null ? null : openBank.trim();
+    }
+
+    public String getComunicationAddr() {
+        return comunicationAddr;
+    }
+
+    public void setComunicationAddr(String comunicationAddr) {
+        this.comunicationAddr = comunicationAddr == null ? null : comunicationAddr.trim();
+    }
+
+    public String getHomeAddr() {
+        return homeAddr;
+    }
+
+    public void setHomeAddr(String homeAddr) {
+        this.homeAddr = homeAddr == null ? null : homeAddr.trim();
+    }
+
+    public String getUrgencyContact() {
+        return urgencyContact;
+    }
+
+    public void setUrgencyContact(String urgencyContact) {
+        this.urgencyContact = urgencyContact == null ? null : urgencyContact.trim();
+    }
+
+    public String getUrgencyContactTelephone() {
+        return urgencyContactTelephone;
+    }
+
+    public void setUrgencyContactTelephone(String urgencyContactTelephone) {
+        this.urgencyContactTelephone = urgencyContactTelephone == null ? null : urgencyContactTelephone.trim();
+    }
+}
